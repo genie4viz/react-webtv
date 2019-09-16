a928c8b4495c92dd5477536c1b288052e837d10c
selistym <selistym@protonmail.com>
2019-09-15 22:39:39 -0700

    init project

---
+++ src/pages/mainpage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Layout } from 'antd';
+import './index.css';
+
+const MainPage = () => {
+  return (
+    <div className="App" >
+      <header className="App-header" >
+
+      </header>
+    </div>
+  );
+}
+
+export default MainPage;
