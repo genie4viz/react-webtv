cd07d45fda4fabf80eea58ede972797927d603b4
selistym <selistym@protonmail.com>
2019-09-17 03:17:53 -0700

    design as fubo

---
+++ src/global/dimens.js
@@ -0,0 +1,2 @@
+export const navHeight = 66;
+export const navFaIconSize = "1.7em";
--- src/pages/ViewPage/index.js
+++ src/pages/ViewPage/index.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Layout, Row, Drawer} from 'antd';
+import {navHeight} from '../../global';
+import {ViewHeader} from '../../components/ViewHeader';
+import background from '../../assets/images/bg.jpg';
 import './index.css';
+
+const { Content } = Layout;
 
 const ViewPage = props => {
     console.log(props.match.params.page, 'params')
+    const [visibleSider, setVisibleSider] = useState(false);
+    const onClose = e => {
+        setVisibleSider(false);
+    }
+
+    const showSider = () => {
+        setVisibleSider(!visibleSider);
+    }
+
     return (
-        <div>View Page</div>
+        <div className="view-frame">
+            <div className="pattern-over"></div>
+            <img className='view-background-img' src={background} alt="" />
+            <Layout className='view-layout'>
+                <ViewHeader onShowSider={showSider}/>
+                <Content className="view-content">
+                    <Row type="flex" justify="center">
+                        <span className="expiration-span">
+                            EXPIRATION: SEPTEMBER 27, 2019
+                        </span>
+                    </Row>
+                    <Drawer
+                        placement="left"
+                        closable={false}
+                        onClose={onClose}
+                        visible={visibleSider}
+                        style={{marginTop: navHeight}}
+                        bodyStyle={{backgroundColor: 'black', height: '100%', color: 'white'}}
+                    >
+                        <h2 style={{color: 'white', padding: 4}}>Categories</h2>
+                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
+                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
+                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
+                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
+                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
+                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
+                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
+                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
+                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
+                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
+                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
+                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
+                    </Drawer>
+                </Content>
+            </Layout>
+        </div>
     )
 }
 
