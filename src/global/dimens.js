c378efb3dd8e9d3f503447e660226868dc420459
selistym <selistym@protonmail.com>
2019-09-19 12:40:18 -0700

    stop on timeline

--- src/global/dimens.js
+++ src/global/dimens.js
@@ -1,2 +1,3 @@
 export const navHeight = 66;
 export const navFaIconSize = "1.7em";
+export const navFaIconSizeSm = "1.4em";
--- src/pages/ViewPage/index.js
+++ src/pages/ViewPage/index.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
-import { Layout, Row, Drawer} from 'antd';
+import { Layout, Row, Col} from 'antd';
 import {navHeight} from '../../global';
 import {ViewHeader} from '../../components/ViewHeader';
+import {Timeline} from '../../components/TimelineAll/Timeline';
 import background from '../../assets/images/bg.jpg';
 import './index.css';
 
@@ -9,49 +10,17 @@ const { Content } = Layout;
 
 const ViewPage = props => {
     console.log(props.match.params.page, 'params')
-    const [visibleSider, setVisibleSider] = useState(false);
-    const onClose = e => {
-        setVisibleSider(false);
-    }
-
-    const showSider = () => {
-        setVisibleSider(!visibleSider);
-    }
 
     return (
         <div className="view-frame">
             <div className="pattern-over"></div>
             <img className='view-background-img' src={background} alt="" />
             <Layout className='view-layout'>
-                <ViewHeader onShowSider={showSider}/>
+                <ViewHeader/>
                 <Content className="view-content">
                     <Row type="flex" justify="center">
-                        <span className="expiration-span">
-                            EXPIRATION: SEPTEMBER 27, 2019
-                        </span>
+                        <Timeline />
                     </Row>
-                    <Drawer
-                        placement="left"
-                        closable={false}
-                        onClose={onClose}
-                        visible={visibleSider}
-                        style={{marginTop: navHeight}}
-                        bodyStyle={{backgroundColor: 'black', height: '100%', color: 'white'}}
-                    >
-                        <h2 style={{color: 'white', padding: 4}}>Categories</h2>
-                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
-                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
-                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
-                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
-                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
-                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
-                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
-                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
-                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
-                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
-                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
-                        <h4 style={{color: 'white', padding: 4}}>Some contents...</h4>
-                    </Drawer>
                 </Content>
             </Layout>
         </div>
